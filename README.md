640442c0e42bb928e4f22c0d51e11806e144ff8a
Ian Rones <ronesp.85@gmail.com>
2019-08-29 23:30:11 -0700

    Added README

---
+++ README.md
@@ -0,0 +1,6 @@
+Ian Rones
+Make School 2019
+CS 1.1 Section D
+
+Assignment: Mad Libs
+Create a random story using inputs from user. 
--- madLibs.py
+++
@@ -1,7 +0,0 @@
-def madLibsStory(name,noun,verb,verb2,adj,adj2,):
-    print("Working")
-    story = f"{name} {verb} his {adj} hand after he {verb} a {adj2} poop."
-    print("Working")
-    return story
-
-madLibsStory('Joe','grill','burned','sees','little','fast')
