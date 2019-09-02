7bc6928d7ed15b44347728f65ddcce912833621b
Ian Rones <ronesp.85@gmail.com>
2019-09-02 12:37:06 -0700

    Updated functionality

--- madLibs.py
+++ madLibs.py
@@ -1,49 +1,46 @@
-# User selects mad libs story
-select_story = input('Which story would you liek to choose: vacation, school, or home?')
-
-
-if select_story == "vacation":
-    first_story()
-# elif select_story =="school":
-def userInput():
-    someVar = input(">> ")
-    return someVar
-
-
-def grab_word():
-    print('Give me a number:')
-    r_number = userInput()
-    print('Enter a noun:')
-    f_noun = userInput()
-    print('Enter a plural noun:')
-    p_noun = userInput()
-    print('Enter an adjective:')
-    f_adj = userInput()
-    print('Enter a verb: ')
-    f_verb = userInput()
-    print('Enter a sport: ')
-    r_sport = userInput()
-    print('Enter a favorite food')
-    f_food = userInput()
-    print('Enter one more noun: ')
-    s_noun = userInput()
-    print('One more adj:')
-    s_adj = userInput()
-    print('Enter another verb: ')
-    s_verb = userInput()
-    print('What body part would you like to use?')
-    p_body = userInput()
-    print('Where would you like this story to take place?')
-    cool_location = userInput()
-
-grab_word()
+nouns = []
+verbs = []
+adj = []
+def list_all_nouns():
+    index = 0
+    for index in nouns:
+        print(index)
+# Function that prints story
 def first_story():
-    print(f'A vacation is when you take a trip to some {f_adj} place. Usually you go to some place 
-        that is near a/an {f_noun}. A good vacation place is one where you can ride {plural_noun} 
-        or play{r_sport}. I like to spend my time {f_verb} or {s_verb}.
-        When parents go on a vacation, they spend their time eating three {f_food} a day, and fathers play golf, and mothers
-        sit around {}. Last summer, my little brother fell in a/an {s_noun} and got poison oak all
-        over his {p_body}. My family is going to go to{cool_location}, and I will practice{s_verb}. Parents
-        need vacations more than kids because parents are always very
-        {s_adj} and because they have to work {r_number} hours every day all year making enough money to pay
-        for the vacation')
+    r_number = input('Enter a number: ')
+    r_sport = input('Enter your favorite sport: ')
+    f_food = input('Enter your favorite food: ')
+    cool_location = input('Where would you like this story to take place? ')
+    p_body = input('Enter a body part: ')
+    f_noun = input('Enter a noun: ')
+    nouns.append(f_noun)
+    p_noun = input('Enter a plural noun: ')
+    nouns.append(p_noun)
+    s_noun = input('Alright, one more noun: ')
+    nouns.append(s_noun)
+    ing_verb = input('Enter a verb ending in ing: ')
+    verbs.append(ing_verb)
+    f_verb = input('Enter a verb: ')
+    verbs.append(f_verb)
+    s_verb = input('Last but not least, one more verb: ')
+    verbs.append(s_verb)
+    f_adj = input('Enter an adj: ')
+    adj.append(f_adj)
+    s_adj = input('One more adj: ')
+    adj.append(s_adj)
+    story = (
+        f'A vacation is when you take a trip to some {f_adj} place. '
+        f'Usually you go to some place that is near a/an {f_noun}. '
+        f'A good vacation place is one where you can ride {p_noun} or play {r_sport}. '
+        f'I like to spend to my time {f_verb} or {s_verb}. '
+        f'When parents go on a vacation they spend their time eating three {f_food} a day, '
+        f'and fathers play golf, and mothers sit around {ing_verb}. Last summer, ym little brother fell in a/an '
+        f'{s_noun} and got poison oak all over his {p_body}. My family is going to {cool_location}, and I will '
+        f'practice {r_sport}. Parents need vacations more than kids because parents are always {s_adj} and because '
+        f'they have to work {r_number} hours every day all year making enough money.'
+    )
+    print(story)
+#print("Hi welcome to my Mad Libs project. After you finish the story you have the option to replace, add, or remove certain words to make the story more interesting. Ready? Here we go!\n")
+first_story()
+#print('A to add a word to the story, L to list the nouns, verbs, or adj used in the story.')
+list_all_nouns()
